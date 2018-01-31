322a56c7b20a049fd0ff4ed7ffc0e5a87965182a
Ubuntu <ubuntu@ip-172-31-22-162.eu-central-1.compute.internal>
2018-01-31 19:38:50 +0000

    finished project

--- my_answers.py
+++ my_answers.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Activation
 from keras.layers import LSTM
 import keras
 
@@ -13,6 +13,10 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    for i in range(0, len(series) - window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size])
+    
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,12 +27,22 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size, 1)))
+    model.add(Dense(1))
+    
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    
+    for pun in punctuation:
+        text.replace(pun, ' ')
 
     return text
 
@@ -37,10 +51,22 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    for i in range(0, len(text) - window_size, step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size])
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    
+    model = Sequential()
+    model.add(LSTM(200, input_shape = (window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam')    
+    
+    return model
